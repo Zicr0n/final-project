f21035581eb6656b79934229c129b5e66cd95a96
Zicr0n <alwanisamir007@gmail.com>
2026-02-25 10:36:06 +0100

    prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,26 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { type ViteDevServer ,defineConfig } from 'vite';
 import { Server } from 'socket.io';
 
-const webSocketServer = {
-	name: 'webSocketServer',
-	configureServer(server: ViteDevServer) {
-		if (!server.httpServer) return
+// const webSocketServer = {
+// 	name: 'webSocketServer',
+// 	configureServer(server: ViteDevServer) {
+// 		if (!server.httpServer) return
 
-		const io = new Server(server.httpServer)
+// 		const io = new Server(server.httpServer)
 
-		io.on('connection', (socket) => {
-			socket.emit('eventFromServer', 'Hello, World 👋')
-			console.log("a user connected")
+// 		io.on('connection', (socket) => {
+// 			socket.emit('eventFromServer', 'Hello, World 👋')
+// 			console.log("a user connected")
 
-			socket.on('chat_message', (msg)=>{
-				console.log("message : " + msg);
-			});
+// 			socket.on('chat_message', (msg)=>{
+// 				console.log("message : " + msg);
+// 			});
 
-			socket.on('disconnect', ()=>{
-				console.log("User Disconnected")
-			})
-		})
-	}
-}
+// 			socket.on('disconnect', ()=>{
+// 				console.log("User Disconnected")
+// 			})
+// 		})
+// 	}
+// }
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), webSocketServer] });
+export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
