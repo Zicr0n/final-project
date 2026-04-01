cf68cf96c7a7cdfe49a158cad1591b153861c6de
unknown <alwanisamir007@gmail.com>
2026-04-01 14:36:34 +0200

    push

--- src/routes/(authenticated)/rooms/[roomId]/+page.server.ts
+++ src/routes/(authenticated)/rooms/[roomId]/+page.server.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { db } from '$lib/server/db';
-import { room,character, character } from '$lib/server/db/schema';
+import { room,character } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
 
 export const load: PageServerLoad = async ({ params, parent, url }) => {
