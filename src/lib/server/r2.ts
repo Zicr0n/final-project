4e364824a8807a6e2d35f22acfc71bf3f61736f4
Zicr0n <alwanisamir007@gmail.com>
2026-04-16 00:21:57 +0200

    added profile pictures

--- src/lib/server/r2.ts
+++ src/lib/server/r2.ts
@@ -1,15 +1,11 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import {
-	R2_ACCOUNT_ID,
-	R2_ACCESS_KEY_ID,
-	R2_SECRET_ACCESS_KEY
-} from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export const r2 = new S3Client({
 	region: 'auto',
-	endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+	endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
 	credentials: {
-		accessKeyId: R2_ACCESS_KEY_ID,
-		secretAccessKey: R2_SECRET_ACCESS_KEY
+		accessKeyId: env.R2_ACCESS_KEY_ID!,
+		secretAccessKey: env.R2_SECRET_ACCESS_KEY!
 	}
 });
