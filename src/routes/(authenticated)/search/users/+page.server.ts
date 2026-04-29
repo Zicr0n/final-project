6bf9618936bdd635d0a7503596aa4382518e9e5e
Zicr0n <alwanisamir007@gmail.com>
2026-04-29 18:25:59 +0200

    fix?

--- src/routes/(authenticated)/search/users/+page.server.ts
+++ src/routes/(authenticated)/search/users/+page.server.ts
@@ -1,8 +1,12 @@
-import type { PageServerLoad } from './$types';
-import { db } from '$lib/server/db';
-import { user } from '$lib/server/db/schema';
+import type { Actions, PageServerLoad } from './$types';
 import { ilike } from 'drizzle-orm';
-import { ne, and } from 'drizzle-orm';
+import { error, redirect } from '@sveltejs/kit';
+import { auth } from '$lib/server/auth';
+import { db } from '$lib/server/db';
+import { user, friendRequest } from '$lib/server/db/schema';
+import { eq, or, and, ne } from 'drizzle-orm';
+import { fail } from '@sveltejs/kit';
+
 
 export const load: PageServerLoad = async ({ url, params, parent }) => {
 	const data = await parent();
@@ -21,4 +25,41 @@ export const load: PageServerLoad = async ({ url, params, parent }) => {
 		.limit(20);
 
 	return { results, query };
+};
+
+export const actions: Actions = {
+	sendRequest: async ({ request, params }) => {
+		const formData = await request.formData()
+		const receiverId = formData.get("otherId");
+
+		const sender = await auth.api.getSession({
+			headers: request.headers
+		});
+
+		if (!sender?.user?.id) {
+			return fail(401, { error: 'Unauthorized' });
+		}
+
+		const senderId = sender.user.id;
+
+		const existing = await db
+			.select()
+			.from(friendRequest)
+			.where(
+				or(
+					and(eq(friendRequest.senderId, senderId), eq(friendRequest.receiverId, receiverId)),
+					and(eq(friendRequest.senderId, receiverId), eq(friendRequest.receiverId, senderId))
+				)
+			)
+			.limit(1);
+
+		if (existing.length) return fail(400, { message: 'Request already exists' });
+
+		await db.insert(friendRequest).values({
+			senderId: senderId,
+			receiverId: receiverId
+		});
+
+	}
+
 };
